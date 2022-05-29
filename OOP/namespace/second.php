48e6833e5ec1d7e733cdd9a55654211976dbe372
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-05-30 00:00:18 +0600

    Add files via upload

---
+++ OOP/namespace/second.php
@@ -0,0 +1,18 @@
+<?php
+namespace second{
+    class A{
+        function __construct()
+        {
+            echo"This is form second namespace construct method<br>";
+        }
+
+        function hello()
+        {
+            echo "this is from second ";
+        }
+    }
+}
+
+
+
+  ?>
