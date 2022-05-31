7362d99b6b3559a33484cd7cc21dbaf9facaaf49
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-05-31 13:56:50 +0600

    Update multidimensionArray.php

--- Array/multidimensionArray.php
+++ Array/multidimensionArray.php
@@ -20,3 +20,18 @@ echo $profession[0][0].' is an '.$profession[0][1].' at '.$profession[0][2]."<br
 ' at '.$profession[2][2]
 ;
 ?>
+echo"<br>";
+
+// using for loop to display all
+
+for($i=0;$i<3;$i++)
+{
+    echo "<p><b>Row number $i</b></p>";
+    echo "<ul>"; 
+    for($j=0;$j<3;$j++)
+    {
+   
+        echo "<li>".$profession[$i][$j]."</li>";
+    }
+    echo "</ul>"; 
+}
