2a9b15b777f8003610e3d3453089898bbfe71dd7
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-05-31 23:48:52 +0600

    Update sortingArrays.php

--- Array/sortingArrays.php
+++ Array/sortingArrays.php
@@ -5,28 +5,31 @@
     arsort() - sort associative arrays in descending order, according to the value
     krsort() - sort associative arrays in descending order, according to the key -->
 <?php
-$browser=array('Chrom','Opera','Firefox');
-//sort($browser);
-rsort($browser);
 
-$a=count($browser);
-for($i=0;$i<$a;$i++)
-{
+    $browser=array('Chrome','Opera','Firefox');
+
+    //sort($browser); // sort arrays in ascending order
+    rsort($browser); // sort arrays in descending order
+
+    $a=count($browser);
+
+    for($i=0;$i<$a;$i++)
+    {
     echo $browser[$i]."<br>";
+    }
 
-}
+    echo"<br>";
 
-echo"<br>";
+    $age = array("Peter"=>"37", "Ben"=>"25", "Joe"=>"43");
 
-$age = array("Peter"=>"37", "Ben"=>"25", "Joe"=>"43");
+    //asort($age); // ascending Ordere according to the value
+    //arsort($age); // decending Order according to the value
 
-//asort($age); // ascending Ordere according to the value
-//arsort($age); // decending Order according to the value
+    ksort($age);   // according to the key
 
-ksort($age);   // according to the key
+    foreach($age as $x => $x_value) {
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
+    }
 
-foreach($age as $x => $x_value) {
-  echo "Key=" . $x . ", Value=" . $x_value;
-  echo "<br>";
-}
 ?>
